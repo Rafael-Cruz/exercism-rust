fdbf2f7658c84f96345ece011d86693a1886659f
Rafael Cruz <rafael.rblc@gmail.com>
2025-08-24 21:34:45 -0300

    Reverse string exercise refactor

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,12 +1,5 @@
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    let mut reversed = String::new();
-    let graphemes = input.graphemes(true).rev();
-
-    for g in graphemes {
-        reversed.push_str(g);
-    }
-
-    reversed
+    input.graphemes(true).rev().collect()
 }
